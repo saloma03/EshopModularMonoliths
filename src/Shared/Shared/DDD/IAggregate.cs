ed96091639e48c122a6bde00d0a0c0a8af83f902
salma <salmaom3r@gmail.com>
2025-09-22 01:48:30 +0200

    feat: Develop Shared Domain for Tactical DDD Abstraction (Entity, Aggreagate, Events)

---
+++ src/Shared/Shared/DDD/IAggregate.cs
@@ -0,0 +1,15 @@
+﻿namespace Shared.DDD
+{
+    public interface IAggregate<T> : IAggregate, IEntity<T>
+    {
+        
+
+    }
+
+    public interface IAggregate : IEntity
+    {
+        IReadOnlyList<IDomainEvent> DomainEvents { get; }
+
+        IDomainEvent[] ClearDomainEvents();
+    }
+}
---
+++ src/Shared/Shared/DDD/IDomainEvent.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace Shared.DDD
+{
+    public interface IDomainEvent: INotification
+    {
+        Guid EventId => Guid.NewGuid();
+
+        public DateTime OccuredOn => DateTime.Now;
+
+        public string EventType => GetType().AssemblyQualifiedName!;
+    }
+}
