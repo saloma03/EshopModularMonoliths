d72633bcc61dbc82c9ab66828abe5b5b05b3a62e
salma <salmaom3r@gmail.com>
2025-09-22 17:58:01 +0200

    feat:  Develop Catalog Domain with Product Domain Entity Models

--- src/Shared/Shared/DDD/Entity.cs
+++ src/Shared/Shared/DDD/Entity.cs
@@ -3,7 +3,7 @@ namespace Shared.DDD
 {
     public abstract class Entity<T> : IEntity<T>
     {
-        public T Id { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public T Id { get; set ; }
         public DateTime? CreatedAt { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
         public string? CreatedBy { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
         public DateTime? LastModified { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
