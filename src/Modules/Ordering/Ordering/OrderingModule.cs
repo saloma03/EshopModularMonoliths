647541dc2542b3df8f83d64d663e181807e9a4b5
salma <salmaom3r@gmail.com>
2026-01-26 18:24:54 +0200

    feat(ordering): implement ordering module and api bootstrapper

--- src/Modules/Ordering/Ordering/OrderingModule.cs
+++ src/Modules/Ordering/Ordering/OrderingModule.cs
@@ -1,33 +1,47 @@
 ﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Ordering.Data;
+using Shared.Data;
+using Shared.Data.Interceptors;
 
-namespace Ordering
+namespace Ordering;
+public static class OrderingModule
 {
-    public static class OrderingModule
+    public static IServiceCollection AddOrderingModule(this IServiceCollection services, 
+        IConfiguration configuration)
     {
-        public static IServiceCollection AddOrderingModule(
-            this IServiceCollection services,
-            IConfiguration configuration
-            )
-        {
+        // Add services to the container.
+        // 1. Api Endpoint services
 
-            return services;
+        // 2. Application Use Case services        
 
-        }
-        public static IApplicationBuilder UseOrderingModule(this IApplicationBuilder app)
+        // 3. Data - Infrastructure services
+        var connectionString = configuration.GetConnectionString("Database");
+
+        services.AddScoped<ISaveChangesInterceptor, AuditableEntityInterceptor>();
+        services.AddScoped<ISaveChangesInterceptor, DispatchDomainEventsInterceptor>();
+
+        services.AddDbContext<OrderingDbContext>((sp, options) =>
         {
-            //add services to the container.
-            //app.
-            //    UseApplicationServicces()
-            //    UseInfrastructureService()
-            //    .UseApiServices();
-            return app;
-        }
+            options.AddInterceptors(sp.GetServices<ISaveChangesInterceptor>());
+            options.UseNpgsql(connectionString);
+        });
+
+        return services;
+    }
+
+    public static IApplicationBuilder UseOrderingModule(this IApplicationBuilder app)
+    {
+        // Configure the HTTP request pipeline.
+        // 1. Use Api Endpoint services
+
+        // 2. Use Application Use Case services
+
+        // 3. Use Data - Infrastructure services
+        app.UseMigration<OrderingDbContext>();
+
+        return app;
     }
 }
